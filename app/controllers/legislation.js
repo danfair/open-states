3a117e21b661b9094414c1e26bc0aa4bdedc302c
Fair, Dan <fair.dan@gmail.com>
2015-03-04 14:43:56 -0600

    Revert a822479..ec02d57
    This rolls back to commit a822479930d4373e05a3b34cc9aa54c167ce9318.

--- app/controllers/legislation.js
+++ app/controllers/legislation.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  list: null,
-  searchTerm: null,
 
-  filterBySearchTerm: function() {
-
-  },
-
-  getData: function(params) {
-    return Ember.$.getJSON('http://openstates.org/api/v1/bills/?apikey=597b1042c52c4b39b80c58c4d441c38a&state=' + params.state_id);
-  }
 });
-
-// update model w/ search term &q=
--- app/routes/legislation/state.js
+++ app/routes/legislation/state.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return Ember.$.getJSON('http://openstates.org/api/v1/bills/?apikey=597b1042c52c4b39b80c58c4d441c38a&state=' + params.state_id);
+    this.set('state', params.state_id);
+  },
+
+  setupController: function(controller, model) {
+    controller.set('state', this.get('state'));
   }
 });
